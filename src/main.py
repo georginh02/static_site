63e1e5801116fa7b086f8cc24a2764d561d8ef27
georginh02 <georgeterzi12@gmail.com>
2026-07-09 18:24:45 +0400

    updated

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 from site_generator import static_to_public
+import os
 
 def main():
-    root = "/home/yaman/bootdev_proj/blog_bd"
+    source_dir = "./static"
+    dest_dir = "./public"
+    print(static_to_public(source_dir, dest_dir))
 main()
--- src/site_generator.py
+++ src/site_generator.py
@@ -1,9 +1,24 @@
 import os, shutil
 
-def static_to_public(path:str):
+def static_to_public(source_dir: str, dest_dir: str) -> list[str]:
     visited = []
-    
-    if os.path.exists(os.path.join(path, "public")):
-        shutil.rmtree(path)
-    else:
-            
+
+    if os.path.exists(dest_dir) and dest_dir.endswith("public"):
+        shutil.rmtree(dest_dir)
+        
+    if os.path.exists(source_dir):
+        items = os.listdir(source_dir)
+        for item in items:
+            source_path = os.path.join(source_dir, item)
+            dest_path = os.path.join(dest_dir, item)
+            visited.append(source_path)
+            if os.path.isfile(source_path):
+                if not os.path.exists(dest_dir):
+                    os.mkdir(dest_dir)
+                shutil.copy(source_path, dest_dir)
+            else:
+                if not os.path.exists(dest_dir):
+                    os.mkdir(dest_dir)
+                visited.extend(static_to_public(source_path, dest_path))
+
+    return visited
