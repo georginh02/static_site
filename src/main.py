395e7bd595356373226b753e4fbc23f9cd9306f2
georginh02 <georgeterzi12@gmail.com>
2026-07-09 18:27:16 +0400

    updated

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from site_generator import static_to_public
-import os
+
+source_dir = "./static"
+dest_dir = "./public"
 
 def main():
-    source_dir = "./static"
-    dest_dir = "./public"
-    print(static_to_public(source_dir, dest_dir))
+    static_to_public(source_dir, dest_dir)
 main()
